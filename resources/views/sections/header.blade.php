45f00df7a07a6e5a840e1a1a954246eecf805766
orrosmanhls <orr@moveohls.com>
2022-04-18 15:33:45 +0300

    styling - header desktop pixel perfect

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,37 +1,35 @@
 <header class="header">
-  <div class="brand">
-    <a href="{{ home_url('/') }}">
-      <img class="brand-image" src="{{ asset('/images/logo.png')  }}" alt="logo">
-      EPICURE
-    </a>
+  <div class="header-left">
+    <div class="brand">
+      <a href="{{ home_url('/') }}">
+        <img class="brand-image" src="{{ asset('/images/logo.png')  }}" alt="logo">
+        <span>EPICURE</span>
+      </a>
+    </div>
+    <div class="pages-nav">
+      <x-button-icon source="{{ asset('../images/mobile-menu.svg') }}" />
+      <?php
+          $args = array(
+              'theme_location'  =>  'pages',
+              'container'       => 'nav',
+              'container_class' => 'pages',
+              'container_id'    => 'pages',
+          );
+          wp_nav_menu($args);
+          ?>
+    </div>
   </div>
 
-  <div class="pages-nav">
-    <x-button-icon source="{{ asset('../images/mobile-menu.svg') }}" />
 
-    <?php
-        $args = array(
-            'theme_location'  =>  'pages',
-            'container'       => 'nav',
-            'container_class' => 'pages',
-            'container_id'    => 'pages',
-        );
-        wp_nav_menu($args);
-        ?>
-  </div>
-
-
-  <div class="user">
-
-    <x-search-input>
-      @section('icon')
-      <x-button-icon source="{{ asset('../images/search-icon.svg') }}" />
-      @endsection
-    </x-search-input>
-
-    <x-button-icon source="{{ asset('../images/user-icon.svg') }}" alt_text="user icon" />
-
-    <x-button-icon source="{{ asset('../images/bag-icon.svg') }}" alt_text="bag icon" />
-
+  <div class="header-right">
+    <div class="user">
+      <x-search-input>
+        @section('icon')
+        <x-button-icon source="{{ asset('../images/search-icon.svg') }}" />
+        @endsection
+      </x-search-input>
+      <x-button-icon source="{{ asset('../images/user-icon.svg') }}" alt_text="user icon" />
+      <x-button-icon source="{{ asset('../images/bag-icon.svg') }}" alt_text="bag icon" />
+    </div>
   </div>
 </header>
