aaee381211d4522b72e2aada264ed2c457284cc0
orrosmanhls <orr@moveohls.com>
2022-04-18 10:53:47 +0300

    styling - chef of the week desktop

--- resources/views/components/chef-front-image.blade.php
+++ resources/views/components/chef-front-image.blade.php
@@ -1,4 +1,4 @@
-<div class="chef-image" style="width:17rem">
-  <img src="{{ $source }}" alt="{{ $alt }}" style="width:17rem">
-  <h2 class="chef-name">{{ $name }}</h2>
+<div class="chef-image" style="width:22rem">
+  <img src="{{ $source }}" alt="{{ $alt }}" style="width:22rem">
+  <h2 class="chef-name text-center">{{ $name }}</h2>
 </div>
