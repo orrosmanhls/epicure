77cfcc27250eece290577e948f0f6d62a5d5bf02
orrosmanhls <orr@moveohls.com>
2022-04-20 15:53:23 +0300

    refractored about us structure

--- resources/views/sections/about-us.blade.php
+++ resources/views/sections/about-us.blade.php
@@ -1,28 +1,32 @@
 <section class="about-us">
   <div class="container">
+
+    <h1>ABOUT US:</h1>
+
     <div class="about-us-info">
-      <h1>ABOUT US:</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacus vel justo fermentum bibendum non
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacus vel justo fermentum bibendum non
         eu ipsum. Cras porta malesuada eros, eget blandit
         turpis suscipit at. Vestibulum sed massa in magna sodales porta. Vivamus elit urna,
         dignissim a vestibulum.
       </p>
-
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacus vel justo fermentum bibendum no
-        eu ipsum. Cras porta malesuada eros.</p>
-
-      <div class="buttons">
-        <x-download-button type="apple" />
-        <x-download-button type="google" />
-      </div>
-
+        eu ipsum. Cras porta malesuada eros.
+      </p>
     </div>
+
     <div class="about-us-brand text-center">
       <a href="/">
         <img src="{{ asset('images/about-logo.png') }}" alt="logo">
         <h2>EPICURE</h2>
       </a>
     </div>
+
+    <div class="about-us-buttons">
+      <x-download-button type="apple" />
+      <x-download-button type="google" />
+    </div>
+
   </div>
 </section>
