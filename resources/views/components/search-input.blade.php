5267cb44f0a21e96875ed989e7d6bf56bbda116e
orrosmanhls <orr@moveohls.com>
2022-05-09 10:55:42 +0300

    search box results

--- resources/views/components/search-input.blade.php
+++ resources/views/components/search-input.blade.php
@@ -1,4 +1,16 @@
 <div class="search">
   @yield('icon')
-  <input class="search-input" type="text" placeholder="Search for restaurants cuisine, chef">
+
+  @php
+  $categories =get_categories();
+  $restaurants = epicure_restaurants_list();
+  @endphp
+
+  <input type="text" class="restaurants-value" value='@json($restaurants)' style="display:none">
+  <input type="text" class="categories-value" value='@json($categories)' style="display:none">
+
+  <input class="search-input" type="text" placeholder="Search for restaurants cuisine, chef" value="">
+  <div class="search-results">
+
+  </div>
 </div>
