4866fa5b4d2c9733ccdcd095ab3b00852fc664ea
orrosmanhls <orr@moveohls.com>
2022-05-12 12:31:51 +0300

    fix: typo

--- resources/views/page-most-popular.blade.php
+++ resources/views/page-most-popular.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="restaurants-page">
+    <section id="restaurants-page">
         @include('sections.restaurants', [
             'restaurants_per_page' => -1,
             'filter' => 'Most Popular',
