0f1769654467c36125f840d6b6ef6ccbbfa18199
orrosmanhls <orr@moveohls.com>
2022-05-15 10:52:35 +0300

    link to coming  soon page

--- resources/views/sections/mobile-menu.blade.php
+++ resources/views/sections/mobile-menu.blade.php
@@ -1,23 +1,23 @@
 <div id="mobile-menu">
-  <div id="mobile-menu-close-btn">
-    <x-button-icon source="{{ asset('../images/x.svg') }}" />
-  </div>
-  <div class="content">
-    @php
-    $args = array(
-    'menu'=> 'pages',
-    'theme_location' => 'pages',
-    'container' => 'nav',
-    'container_class' => 'pages',
-    'container_id' => 'pages',
-    );
-    wp_nav_menu($args);
-    @endphp
-    <div class="divider"></div>
-    <div class="links">
-      <a href="">Sign in</a>
-      <a href="">Contact us</a>
-      <a href="">Terms of Use</a>
+    <div id="mobile-menu-close-btn">
+        <x-button-icon source="{{ asset('../images/x.svg') }}" />
     </div>
-  </div>
+    <div class="content">
+        @php
+            $args = [
+                'menu' => 'pages',
+                'theme_location' => 'pages',
+                'container' => 'nav',
+                'container_class' => 'pages',
+                'container_id' => 'pages',
+            ];
+            wp_nav_menu($args);
+        @endphp
+        <div class="divider"></div>
+        <div class="links">
+            <a href="/coming-soon">Sign in</a>
+            <a href="/coming-soon">Contact us</a>
+            <a href="/coming-soon">Terms of Use</a>
+        </div>
+    </div>
 </div>
