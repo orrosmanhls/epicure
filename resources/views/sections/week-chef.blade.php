191efeaaaddb08b5ec6a9855d910e950b9aba5d2
orrosmanhls <orr@moveohls.com>
2022-05-15 12:42:19 +0300

    fix: open menu

--- resources/views/sections/week-chef.blade.php
+++ resources/views/sections/week-chef.blade.php
@@ -1,27 +1,27 @@
 <section class="chef-of-the-week">
 
-  <h1 class="text-center">CHEF OF THE WEEK:</h1>
+    <h1 class="text-center">CHEF OF THE WEEK:</h1>
 
-  @php $chef =epicure_random_chef() @endphp
+    @php $chef =epicure_random_chef() @endphp
 
-  <div class="chef-info">
-    <x-chef-front-image source="{{ $chef->image }}" alt="chef" name="{{ $chef->name }}" />
-    <p>{{ $chef->info }}</p>
-  </div>
+    <div class="chef-info">
+        <x-chef-front-image source="{{ $chef->image }}" alt="chef" name="{{ $chef->name }}" />
+        <p>{{ $chef->info }}</p>
+    </div>
 
-  <div class="container">
+    <div class="container">
 
-    <h2>{{ explode(' ', $chef->name)[0] }}'s restaurants:</h2>
+        <h2>{{ explode(' ', $chef->name)[0] }}'s restaurants :</h2>
 
-    <ul class="restaurants-list">
-      @foreach($chef->restaurants as $restaurant)
-      <li class="restaurant-card">
-        <x-restaurant-card source="{{ $restaurant->image }}" name="{{ $restaurant->name }}" />
-      </li>
-      @endforeach
-    </ul>
+        <ul class="restaurants-list">
+            @foreach ($chef->restaurants as $restaurant)
+                <li class="restaurant-card">
+                    <x-restaurant-card source="{{ $restaurant->image }}" name="{{ $restaurant->name }}" />
+                </li>
+            @endforeach
+        </ul>
 
-  </div>
+    </div>
 
 
 </section>
