8cde41e813a07a0414a8af7feee986be1b7a84b4
orrosmanhls <orr@moveohls.com>
2022-05-15 11:57:26 +0300

    fix: download buttons redirect to "coming soon"

--- resources/views/components/download-button.blade.php
+++ resources/views/components/download-button.blade.php
@@ -1,14 +1,15 @@
 <div class="download-button text-center">
-  <a href="#">
-    @switch($type)
-    @case('apple')
-    <img src="{{ asset('/images/apple-store.svg') }}" alt="{{ $type }}">
-    <span>Download on the <div>App Store</div></span>
-    @break
-    @case('google')
-    <img src="{{ asset('/images/google-play.svg')}}" alt="{{ $type }}">
-    <span>Get it on <div>Google Play</div></span>
-    @break
-    @endswitch
-  </a>
+    <a href="/coming-soon">
+        @switch($type)
+            @case('apple')
+                <img src="{{ asset('/images/apple-store.svg') }}" alt="{{ $type }}">
+                <span>Download on the <div>App Store</div></span>
+            @break
+
+            @case('google')
+                <img src="{{ asset('/images/google-play.svg') }}" alt="{{ $type }}">
+                <span>Get it on <div>Google Play</div></span>
+            @break
+        @endswitch
+    </a>
 </div>
